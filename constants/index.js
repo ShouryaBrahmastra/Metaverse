8faeb3b0e017d4e3c2c3f4738f8c1795ed2e08c5
Shourya Gupta <gptshourya02@gmail.com>
2022-12-18 18:19:26 +0530

    first commit

---
+++ constants/index.js
@@ -0,0 +1,77 @@
+export const exploreWorlds = [
+  {
+    id: 'world-1',
+    imgUrl: '/planet-01.png',
+    title: 'The Hogwarts',
+  },
+  {
+    id: 'world-2',
+    imgUrl: '/planet-02.png',
+    title: 'The Upside Down',
+  },
+  {
+    id: 'world-3',
+    imgUrl: '/planet-03.png',
+    title: 'Kadirojo Permai',
+  },
+  {
+    id: 'world-4',
+    imgUrl: '/planet-04.png',
+    title: 'Paradise Island',
+  },
+  {
+    id: 'world-5',
+    imgUrl: '/planet-05.png',
+    title: 'Hawkins Labs',
+  },
+];
+
+export const startingFeatures = [
+  'Find a world that suits you and you want to enter',
+  'Enter the world by reading to be safe',
+  'No need to beat around the bush, just stay on the gas and have fun',
+];
+
+export const newFeatures = [
+  {
+    imgUrl: '/vrpano.svg',
+    title: ':- A new world',
+    subtitle:
+        'we have the latest update with new world for you to try never mind',
+  },
+  {
+    imgUrl: '/headset.svg',
+    title: ':- More realistic',
+    subtitle:
+        'In the latest update, your eyes are narrow, making the world more realistic than ever',
+  },
+];
+
+export const insights = [
+  {
+    imgUrl: '/planet-06.png',
+    title: 'Elon Musk says metaverse isn’t ‘compelling,’',
+    subtitle:
+        'Elon Musk doesn’t find the idea of the metaverse “compelling” — and thinks that bulky headsets being pushed by Mark Zuckerberg and other proponents of virtual reality are too awkward to catch on. ',
+  },
+  {
+    imgUrl: '/planet-07.png',
+    title: '7 tips to easily master the madness of the Metaverse',
+    subtitle:
+        'Bear in mind while reading The Rules are axiomatic, hypotheses, distilled from decades of experience and insights from many practitioners, embraced by many in principle.',
+  },
+  {
+    imgUrl: '/planet-08.png',
+    title: 'With one platform you can explore the whole world virtually',
+    subtitle:
+        'Like the internet, the metaverse will help you connect with people when you aren’t physically in the same place and get us even closer to that feeling of being together in person.',
+  },
+];
+
+export const socials = [
+  {
+    name: 'instagram',
+    url: '/instagram.svg',
+    link: 'https://www.instagram.com/shouryagupta2002/',
+  },
+];
